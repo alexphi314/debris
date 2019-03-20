899aec4abbd103ada3cf1477f96eb6e8b54b3d9f
alexphi <alexphi@umich.edu>
2019-03-20 16:42:50 -0400

    Added a few necessary tests.
    Added new plots

--- test.py
+++ test.py
@@ -34,9 +34,17 @@ class Test_object_init(unittest.TestCase):
     M = math.radians(292.7424)
     n = 10.84775486 * 2 * math.pi / 86400
 
+    line0 = '0 VANGUARD 1'
     line1 = '1     5U 58002B   19075.71745479 -.00000160  00000-0 -17930-3 0  9998'
     line2 = '2     5  34.2430 201.2113 1845233  88.2396 292.7424 10.84775486155534'
-    str = '{}\n{}'.format(line1, line2)
+    str = '{}\n{}\n{}'.format(line0, line1, line2)
+
+    timeSincePerigee = M/n
+    perigee_time = epoch - dt.timedelta(seconds=timeSincePerigee)
+
+    a = 8620026.476
+    h = 57610384007.89
+    P = 8326524.503
 
     def test_function_tle_str(self):
         obj = tle_parse.Object(self.str)
@@ -48,6 +56,12 @@ class Test_object_init(unittest.TestCase):
         self.assertAlmostEqual(self.wp,obj.wp,5)
         self.assertAlmostEqual(self.M,obj.M,5)
         self.assertAlmostEqual(self.n,obj.n,5)
+        self.assertAlmostEqual(self.a,obj.a,3)
+        self.assertAlmostEqual(self.h,obj.h,3)
+        self.assertAlmostEqual(self.P,obj.P,3)
+        self.assertEqual(self.perigee_time, obj._pt)
+        self.assertFalse(obj.deb)
+        self.assertEqual('VANGUARD 1',obj.satName)
 
     def test_function_kepElems(self):
         E = tle_parse.solve_kepler(self.M, self.e)
@@ -78,6 +92,28 @@ class Test_object_init(unittest.TestCase):
         self.assertEqual(obj.a, tle_obj.a)
         self.assertFalse(obj.deb)
 
+    def test_deb_classification(self):
+        line0 = '0 THOR ABLESTAR R/B'
+        line1 = '1    47U 60007C   19078.62082020 -.00000047 +00000-0 +14842-4 0  9994'
+        line2 = '2    47 066.6649 293.7858 0235398 276.6054 080.8306 14.42023965068951'
+        str = '{}\n{}\n{}\n'.format(line0, line1, line2)
+        obj = tle_parse.Object(tle_str=str)
+        self.assertTrue(obj.deb)
+
+        line0 = '0 ECHO 1 DEB (METAL OBJ)'
+        line1 = '1    53U 60009E   19078.45142974 -.00000010 +00000-0 +73023-3 0  9994'
+        line2 = '2    53 047.2747 225.1950 0099831 173.3107 186.9037 12.17225727613029'
+        str = '{}\n{}\n{}\n'.format(line0, line1, line2)
+        obj = tle_parse.Object(tle_str=str)
+        self.assertTrue(obj.deb)
+
+        line0 = '0 SOLRAD 1 (GREB)'
+        line1 = '1    46U 60007B   19078.71696926 -.00000006 +00000-0 +21930-4 0  9990'
+        line2 = '2    46 066.6907 197.8491 0203173 111.6021 250.6868 14.49212027073077'
+        str = '{}\n{}\n{}\n'.format(line0, line1, line2)
+        obj = tle_parse.Object(tle_str=str)
+        self.assertFalse(obj.deb)
+
 class Test_solve_kepler(unittest.TestCase):
     def test_function(self):
         E = math.radians(85)
@@ -96,7 +132,7 @@ class Test_calc_o(unittest.TestCase):
         o_out = tle_parse.calc_o(E,e)
         self.assertAlmostEqual(o,o_out,2)
 
-class Test_peri2geo(unittest.TestCase):
+class Test_peri2eci(unittest.TestCase):
     def test_function(self):
         i = math.radians(30)
         O = math.radians(40)
--- tle_parse.py
+++ tle_parse.py
@@ -3,11 +3,13 @@ import datetime as dt
 import argparse
 import pickle
 import os
+import re
 
 import numpy as np
 from bokeh.plotting import figure, output_file, show, ColumnDataSource
 from bokeh.models import HoverTool, Legend, LogColorMapper, ColorBar, LogTicker
 from bokeh.layouts import gridplot
+from bokeh.palettes import Blues9
 
 class Object:
     """
@@ -45,8 +47,18 @@ class Object:
             self.deb = kepElems['deb']
         else:
             lines = tle_str.split('\n')
-            line1 = lines[0]
-            line2 = lines[1]
+            line0 = lines[0]
+            line1 = lines[1]
+            line2 = lines[2]
+
+            self.satName = line0[2:].strip()
+            rb_re = re.compile('\S*R/B\S*',re.IGNORECASE)
+            deb_re = re.compile('\S*DEB\S*',re.IGNORECASE)
+
+            if rb_re.search(self.satName) or deb_re.search(self.satName):
+                self.deb = True
+            else:
+                self.deb = False
 
             self.satNum = int(line1[2:7])
             epoch_year = line1[18:20]
@@ -221,18 +233,29 @@ def peri2eci(O, i, wp):
     Q = np.array([[q11, q12, q13], [q21, q22, q23], [q31, q32, q33]])
     return Q
 
-def parse_catalog():
+def parse_catalog(format):
     """
     Open the txt file with all TLEs in the sat cat, create an Object for each tle
+    :param int format: 2 or 3 corresponding to 2 line or 3 line format
     :return: objects list, each index is an Object in the sat cat
     """
-    with open('satcat.txt','r') as f:
+    if format == 2:
+        file = 'satcat.txt'
+    else:
+        file = 'satcat_3le.txt'
+
+    with open(file,'r') as f:
         lines = f.readlines()
-        objects = [None]*int(len(lines)/2)
-        for i in range(0, len(lines), 2):
-            tle = lines[i:i+2]
-            str = '{}{}'.format(tle[0], tle[1])
-            objects[int(i/2)] = Object(tle_str=str)
+        objects = [None]*int(len(lines)/format)
+        for i in range(0, len(lines), format):
+            tle = lines[i:i+format]
+
+            if format == 2:
+                str = '{}{}'.format(tle[0],tle[1])
+            else:
+                str = '{}{}{}'.format(tle[0], tle[1], tle[2])
+
+            objects[int(i/format)] = Object(tle_str=str)
 
     return objects
 
@@ -241,17 +264,17 @@ Re = 6378.37e3 # m
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Input arguments")
-    required = parser.add_argument_group('optional arguments')
-    required.add_argument("--parse_data", "-p", help="Re-parse sat cat file, rather than read stored data",
+    optional = parser.add_argument_group('optional arguments')
+    optional.add_argument("--parse_data", "-p", help="Re-parse sat cat file, rather than read stored data",
                           required=False, action="store_true", default=False)
     #optional = parser.add_argument_group('optional arguments')
     args = vars(parser.parse_args())
 
     parse = args['parse_data']
-    savefile = 'satcat.pickle'
+    savefile = 'satcat3.pickle'
     if parse or savefile not in os.listdir(os.getcwd()):
         print('Parsing tle file...')
-        objects = parse_catalog()
+        objects = parse_catalog(3)
         with open(savefile,'wb') as f:
             pickle.dump(objects,f)
     else:
@@ -268,16 +291,28 @@ if __name__ == "__main__":
     alt_delim = 50
     lower_i = 0
     upper_i = 180
-    i_delim = 5
+    i_delim = 1
 
     np_alt = np.linspace(lower_alt, upper_alt, int((upper_alt+alt_delim-lower_alt)/alt_delim))
     altitudes = dict.fromkeys(list(np_alt),0)
+    deb_altitudes = dict.fromkeys(list(np_alt),0)
     np_inc = np.linspace(lower_i, upper_i, int((upper_i+i_delim-lower_i)/i_delim))
     inclinations = dict.fromkeys(list(np_inc),0) #Range from 0 to 180 deg inclin. in 5 deg steps
+    deb_inclinations = dict.fromkeys(list(np_inc),0)
 
     z = np.zeros([len(np_inc),len(np_alt)])
+    deb_z = np.zeros([len(np_inc),len(np_alt)])
 
+    x_vals = []
+    y_vals = []
+    deb_x_vals = []
+    deb_y_vals = []
+
+    debCounter = 0
     for obj in objects:
+        if obj.deb:
+            debCounter += 1
+
         inc = math.degrees(obj.i)
         alt = (obj.a - Re)/1000 #km
 
@@ -287,8 +322,14 @@ if __name__ == "__main__":
         if nearest_alt >= lower_alt and nearest_alt <= upper_alt:
             altitudes[nearest_alt]+= 1
 
+            if obj.deb:
+                deb_altitudes[nearest_alt]+=1
+
         if nearest_inc >= lower_i and nearest_inc <= upper_i:
             inclinations[nearest_inc]+=1
+
+            if obj.deb:
+                deb_inclinations[nearest_inc]+=1
 
         ## We need both within range to generate the image array
         if nearest_alt < lower_alt or nearest_alt > upper_alt \
@@ -299,14 +340,26 @@ if __name__ == "__main__":
         y_coord = np.where(np_inc == nearest_inc)[0][0]
 
         z[y_coord][x_coord]+= 1
+        x_vals.append(x_coord)
+        y_vals.append(y_coord)
 
+        if obj.deb:
+            deb_z[y_coord][x_coord]+= 1
+            deb_x_vals.append(x_coord)
+            deb_y_vals.append(y_coord)
 
-    cmap = LogColorMapper(palette='Viridis256',low=1,high=z.max())
+    print('{} TLEs, with {} deb'.format(len(objects),debCounter))
+    Blues9.reverse()
+    cmap = LogColorMapper(palette=Blues9,low=1,high=z.max())
     output_file('Plots/tle_distro.html')
-    p = figure(title='Distribution of TLEs', x_axis_label='Altitude (km)', y_axis_label='Debris Amount')
+
+    ## All TLE plots
+    p = figure(title='Distribution of TLEs', x_axis_label='Altitude (km)', y_axis_label='TLE Amount',
+               tooltips=[("x","$x"),("y","$y")])
     p.vbar(x=list(altitudes.keys()), top=list(altitudes.values()), bottom=0, width=alt_delim)
 
-    p2 = figure(title='Distribution of TLEs', x_axis_label='Inclination (deg)', y_axis_label='Debris Amount')
+    p2 = figure(title='Distribution of TLEs', x_axis_label='Inclination (deg)', y_axis_label='TLE Amount',
+                tooltips=[("x","$x"),("y","$y")])
     p2.vbar(x=list(inclinations.keys()), top=list(inclinations.values()), bottom=0, width=i_delim)
 
     p3 = figure(title='Distribution of TLEs',x_axis_label='Altitude (km)',y_axis_label='Inclination (deg)',
@@ -315,5 +368,30 @@ if __name__ == "__main__":
     p3.image(image=[z],x=lower_alt,y=lower_i,dw=upper_alt-lower_alt,dh=upper_i-lower_i,color_mapper=cmap)
     color_bar = ColorBar(color_mapper=cmap,location=(0,0),ticker=LogTicker())
     p3.add_layout(color_bar,'right')
-    grid = gridplot([[p, p2],[p3]])
+
+    p4 = figure(title='Distribution of TLEs',x_axis_label='Altitude (km)',y_axis_label='Inclination (deg)',
+                tooltips=[("x","$x"),("y","$y")])
+    p4.scatter(x=x_vals,y=y_vals,marker='circle')
+
+    ## Debris plots
+    p5 = figure(title='Distribution of Debris', x_axis_label='Altitude (km)', y_axis_label='Debris Amount',
+                tooltips=[("x","$x"),("y","$y")])
+    p5.vbar(x=list(deb_altitudes.keys()), top=list(deb_altitudes.values()), bottom=0, width=alt_delim)
+
+    p6 = figure(title='Distribution of Debris', x_axis_label='Inclination (deg)', y_axis_label='Debris Amount',
+                tooltips=[("x","$x"),("y","$y")])
+    p6.vbar(x=list(deb_inclinations.keys()), top=list(deb_inclinations.values()), bottom=0, width=i_delim)
+
+    p7 = figure(title='Distribution of Debris', x_axis_label='Altitude (km)', y_axis_label='Inclination (deg)',
+                x_range=(lower_alt, upper_alt), y_range=(lower_i, upper_i),
+                tooltips=[("x", "$x"), ("y", "$y"), ("value", "@image")])
+    p7.image(image=[z], x=lower_alt, y=lower_i, dw=upper_alt - lower_alt, dh=upper_i - lower_i, color_mapper=cmap)
+    color_bar = ColorBar(color_mapper=cmap, location=(0, 0), ticker=LogTicker())
+    p7.add_layout(color_bar, 'right')
+
+    p8 = figure(title='Distribution of Debris', x_axis_label='Altitude (km)', y_axis_label='Inclination (deg)',
+                tooltips=[("x", "$x"), ("y", "$y")])
+    p8.scatter(x=deb_x_vals, y=deb_y_vals, marker='circle')
+
+    grid = gridplot([[p, p2],[p3, p4], [p5, p6], [p7, p8]])
     show(grid)
